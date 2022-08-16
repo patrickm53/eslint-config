bf4f09083352f93dd2fced11f68e44ba2161aa98
patrickm53 <patrickmcenaney53@gmail.com>
2022-08-16 14:03:20 -0300

    Disable "import/order" and update "simple-import-sort" groups

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const importRules = {
   'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   'import/no-import-module-exports': 'off',
   'import/no-named-as-default': 'off',
+  'import/order': 'off',
   'import/prefer-default-export': 'off',
 };
 
@@ -238,13 +239,14 @@ module.exports = {
       {
         groups: [
           ['^\\u0000'], // Side effect imports
-          ['^react$', '^react-dom$', '^react', '^@?\\w'],
-          ['^(src/)?modules'],
-          ['^(src/)?config', '^(src/)?literals'],
-          ['^(src/)?actions', '^(src/)?reducers', '^(src/)?sagas'],
-          ['^(src/)?components', '^(src/)?containers', '^(src/)?routes'],
-          ['^(src/)?types'],
-          ['^test'],
+          [`^(${require('module').builtinModules.join('|')})(/|$)`],
+          ['^react$', '^react-dom$', '^react', '^@?(?!test/)\\w'],
+          ['^~/modules'],
+          ['^~/config'],
+          ['^~/actions', '^~/literals', '^~/reducers', '^~/services', '^~/sagas'],
+          ['^~/components', '^~/containers', '^~/routes'],
+          ['^~'],
+          ['^test/\\w'],
           ['^\\./[^.]'], // './*'
           ['^\\.\\./'], // '../*'
         ],
