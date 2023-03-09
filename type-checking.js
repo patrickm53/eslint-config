797f88c9e120af92a807ef426170accb952a16ba
patrickm53 <patrickmcenaney53@gmail.com>
2023-03-08 22:18:30 -0300

    Update type-checking `project` path

--- type-checking.js
+++ type-checking.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       files: ['**/*.ts?(x)'],
       parserOptions: {
-        project: '**/*/tsconfig.json',
+        project: './tsconfig.json',
       },
       extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
     },
